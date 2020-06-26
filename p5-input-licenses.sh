6c062877a6b6f4f907367973d1f8428d2cc7b1f4
Mac VFX <macvfx@me.com>
2020-06-26 14:20:23 -0700

    schells cript
    nsdchat srvinfo

--- p5-input-licenses.sh
+++ p5-input-licenses.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
 /usr/local/aw/bin/nsdchat -c License resources > /private/tmp/p5input-licenses.txt ; open /private/tmp/p5input-licenses.txt
+
+/usr/local/aw/bin/nsdchat -c srvinfo hostid > /private/tmp/p5input-hostid.txt ; open /private/tmp/p5input-hostid.txt
+
+/usr/local/aw/bin/nsdchat -c srvinfo version > /private/tmp/p5input-version.txt ; open /private/tmp/p5input-version.txt
+
+/usr/local/aw/bin/nsdchat -c srvinfo port > /private/tmp/p5input-port.txt ; open /private/tmp/p5input-port.txt
+
+/usr/local/aw/bin/nsdchat -c srvinfo hostname > /private/tmp/p5input-hostname.txt ; open /private/tmp/p5input-hostname.txt
+
+/usr/local/aw/bin/nsdchat -c srvinfo platform > /private/tmp/p5input-platform.txt ; open /private/tmp/p5input-platform.txt
+
+/usr/local/aw/bin/nsdchat -c srvinfo server > /private/tmp/p5input-server.txt ; open /private/tmp/p5input-server.txt
+
+/usr/local/aw/bin/nsdchat -c srvinfo lexxvers > /private/tmp/p5input-lexxvers.txt ; open /private/tmp/p5input-lexxvers.txt
+
+/usr/local/aw/bin/nsdchat -c srvinfo buildstamp > /private/tmp/p5input-buildstamp.txt ; open /private/tmp/p5input-buildstamp.txt
+
+/usr/local/aw/bin/nsdchat -c srvinfo address > /private/tmp/p5input-address.txt ; open /private/tmp/p5input-address.txt
+
